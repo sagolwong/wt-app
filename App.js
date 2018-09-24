91265c43218d4448ba4907a30ac828e573441511
sagolwong <5910110678@psu.ac.th>
2018-09-24 09:57:05 +0700

    แสดงข้อความ hello

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  doIt = () =>{
+    console.log("Hello from console")
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text onPress={this.doIt}>Hello World</Text>
       </View>
     );
   }
