3904627413e289b17ecef509d232a0d37c4ec6cd
sagolwong <5910110678@psu.ac.th>
2018-09-28 15:53:51 +0700

    connect เข้ากับ api openweathermap

--- components/Forcast.js
+++ components/Forcast.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import { white } from 'ansi-colors';
 
 export default class Forcast extends React.Component {
 
   render() {
     return (
-      <View>
-          <Text>{this.props.main}</Text>
-          <Text>{this.props.description}</Text>
-          <Text>{this.props.temp}</Text>
-          <Text>ํC</Text>
+      <View style={styles.container}>
+          <Text style={styles.main}>{this.props.main}</Text>
+          <Text style={styles.des}>{this.props.description}</Text>
+          <Text style={styles.temp}>{this.props.temp} ํC</Text>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+    container: { 
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    main:{
+        fontSize: 25 ,
+        paddingTop: 15,
+        color : 'white',
+    },
+    des:{
+      fontSize: 15 ,
+      paddingTop: 25,
+      color : 'white',
+    },
+    temp:{
+      fontSize: 25 ,
+      paddingTop: 30,
+      paddingBottom:25,
+      color : 'white',
+    },
+});
--- components/Weather.js
+++ components/Weather.js
@@ -7,16 +7,38 @@ export default class Weather extends React.Component {
      super(props);
      this.state={
          forecast:{
-             main:'-',description:'-',temp:0
+             main:'main',description:'description',temp:0
          }
      }
  }
+ fetchData = () => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.zipCode},th&units=metric&APPID=fd68c0f2039c5a25f666a9ff374bc93e`)
+   .then((response) => response.json())
+   .then((json) => {
+     this.setState(
+      {
+         forecast: {
+           main: json.weather[0].main,
+           description: json.weather[0].description,
+           temp: json.main.temp
+         }
+      });
+  })
+   .catch((error) => {
+      console.warn(error);
+   });
+  }
+  componentDidMount = () => this.fetchData()
+
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
-          <Text>Zip code is {this.props.zipCode}.</Text>
-          <Forecast {...this.state.forecast} />
+          <View style={styles.con}>
+            <Text style={styles.etext}>Zip code is {this.props.zipCode}.</Text>
+            <Forecast {...this.state.forecast} />
+          </View>
+          
         </ImageBackground>
       </View>
     );
@@ -26,5 +48,18 @@ export default class Weather extends React.Component {
 const styles = StyleSheet.create({
     container: { paddingTop: 25 },
     backdrop: { width: '100%', height: '100%'},
+    con:{
+      flexDirection: 'column', 
+      justifyContent:'center',
+      alignItems: 'center',
+      backgroundColor: 'black',
+      opacity:0.4,
+     },
+    etext:{
+      fontSize: 15,
+      paddingTop: 25,
+      color : 'white',
+    }
+    
 });
 
